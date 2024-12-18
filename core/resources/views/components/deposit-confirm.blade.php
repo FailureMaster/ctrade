a6d7c84ce8e01f74b2db4cc058f2c4079df44372
FailureMaster <mike.macabulos@gmail.com>
2024-12-18 05:23:32 +0000

    make deposit payment on mobile

--- core/resources/views/components/deposit-confirm.blade.php
+++ core/resources/views/components/deposit-confirm.blade.php
@@ -2,7 +2,7 @@
     @csrf
     <div class="row">
         <div class="col-md-12 text-center">
-            <p class="text-center mt-2">
+            {{-- <p class="text-center mt-2">
                 @lang('You have requested') <b class="text--success">
                     {{ showAmount($data['amount']) }}
                     {{ __(@$data->method_currency) }}</b> , @lang('Please pay')
@@ -11,14 +11,33 @@
                     <span data-bs-toggle="tooltip"  title="@lang('Charge')">{{ showAmount($data['charge']) }}</span> =
                     {{ showAmount($data['final_amo']) . ' ' . $data['method_currency'] }}
                 </b> @lang('for successful payment')
-            </p>
+            </p> --}}
+            @if( $data->gateway->message == null )
+                <p class="text-center mt-2">
+                    @lang('You have requested') <b class="text--success">
+                        {{ showAmount($data['amount']) }}
+                        {{ __(@$data->method_currency) }}</b> , @lang('Please pay')
+                    <b class="text--success">
+                        {{ showAmount($data['amount']) }} +
+                        <span data-bs-toggle="tooltip"  title="@lang('Charge')">{{ showAmount($data['charge']) }}</span> =
+                        {{ showAmount($data['final_amo']) . ' ' . $data['method_currency'] }}
+                    </b> @lang('for successful payment')
+                </p>
+            @else
+                <p>{{ $data->gateway->message }}</p>
+            @endif
             <h4 class="text-center my-4">@lang('Please follow the instruction below')</h4>
             <p class="my-4 text-center">@php echo  $data->gateway->description @endphp</p>
         </div>
         <x-viser-form identifier="id" identifierValue="{{ $gateway->form_id }}" />
         <input type="hidden" name="trx" value="{{ $trx }}">
-        <div class="col-md-12">
+        {{-- <div class="col-md-12">
             <button type="submit" class="btn btn--base w-100">@lang('Pay Now')</button>
-        </div>
+        </div> --}}
+        @if( $data->gateway->allow_pay )
+            <div class="col-md-12">
+                <button type="submit" class="btn btn--base w-100">@lang('Pay Now')</button>
+            </div>
+        @endif
     </div>
 </form>
