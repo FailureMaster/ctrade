8df987f4a09a99a3e4f076ad8da49a24de7b95d0
FailureMaster <mike.macabulos@gmail.com>
2024-12-19 03:53:47 +0000

    added global scope on notification

--- core/app/Models/AdminNotification.php
+++ core/app/Models/AdminNotification.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\ExcludeUserScope;
 use Illuminate\Database\Eloquent\Model;
 
 class AdminNotification extends Model
@@ -9,5 +10,10 @@ class AdminNotification extends Model
     public function user()
     {
     	return $this->belongsTo(User::class);
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new ExcludeUserScope('admin_notifications.user_id'));
     }
 }
