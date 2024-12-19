af3bc450068860a8d9a19479a0a1385504f613b4
FailureMaster <mike.macabulos@gmail.com>
2024-12-19 03:54:41 +0000

    added new condition on global scope display only users with their assign leads

--- core/app/Scopes/ExcludeUserScope.php
+++ core/app/Scopes/ExcludeUserScope.php
@@ -2,10 +2,12 @@
 
 namespace App\Scopes;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Support\Facades\Auth;
+
 
 class ExcludeUserScope implements Scope
 {
@@ -27,7 +29,21 @@ class ExcludeUserScope implements Scope
 
         // Apply condition to exclude user with ID 1
         if( $authUser <> null ){
-            $builder->where($this->column, '!=', 1);
+           
+            // Ownership data
+            $includeUserIds = User::withoutGlobalScopes()->where('owner_id', $authUser->id)->pluck('id')->toArray();
+
+            $excludedUserIds = User::withoutGlobalScopes()->where('account_type', 'test')->pluck('id')->toArray();
+            
+            if( $authUser->permission_group_id == 1 ){
+                $builder->whereNotIn($this->column, array_merge($excludedUserIds, [1]));
+            }
+            else{
+                $builder->whereNotIn($this->column, array_merge($excludedUserIds, [1]))->whereIn($this->column, $includeUserIds);
+            }
+            
+            // $builder->whereNotIn($this->column, array_merge($excludedUserIds, [1000]))->whereIn($this->column, $includeUserIds);
+            // $builder->where($this->column, '!=', 1);
         }
     }
 }
